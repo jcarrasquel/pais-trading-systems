4fdc7866bfb9d99c4e086932684fe80d2bd864cf
jcarrasquel <jcarrasquel@hse.ru>
2019-08-27 19:06:39 +0300

    Project adding

---
+++ pais-tradingsystems-eventlog-gen-order/src/main/java/com/pais/fix/EventLog.java
@@ -0,0 +1,18 @@
+package com.pais.fix;
+
+
+import java.util.LinkedHashMap;
+
+public class EventLog {
+	
+	private LinkedHashMap<String, Case> cases;
+	
+	public EventLog(){
+		cases = new LinkedHashMap<String, Case>();
+	}
+
+	public LinkedHashMap<String, Case> getCases() {
+		return cases;
+	}
+
+}
